a310ad711ec7d33e2feae0152f0b21479218a8a6
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-01-02 21:20:32 +0000

    adding @Click support, changing @View to @UiView to avoid import errors with Android View

--- src/main/java/com/googlecode/androidannotations/annotations/Click.java
+++ src/main/java/com/googlecode/androidannotations/annotations/Click.java
@@ -21,13 +21,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Should be used on View fields in activity classes
+ * Should be used on click listener methods in activity classes
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface View {
-	public static final int DEFAULT_VALUE = -1;
-
-	int value() default DEFAULT_VALUE;
+@Target(ElementType.METHOD)
+public @interface Click {
+	int value();
 }
