e7d7637f671683069c4acf48e4d47a00ce5bec9b
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-01-04 07:14:35 +0000

    creating the @Value annotation, the javadoc and the associated example

--- HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
+++ HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
@@ -3,9 +3,11 @@ package com.googlecode.androidannotations.helloworldeclipse;
 import android.app.Activity;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.Layout;
+import com.googlecode.androidannotations.annotations.Value;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 @Layout(R.layout.main)
@@ -17,10 +19,25 @@ public class MyActivity extends Activity {
 	@ViewById(R.id.myTextView)
 	TextView textView;
 
+	@Value(R.string.hello)
+	String helloFormat;
+
+	@Value
+	String[] bestFoods;
+
+	@Value
+	int androidColor;
+
 	@Click
 	void myButton() {
 		String name = myEditText.getText().toString();
-		textView.setText("Hello " + name);
+		String message = String.format(helloFormat, name);
+		textView.setText(message);
+		textView.setTextColor(androidColor);
+
+		for (String item : bestFoods) {
+			Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
+		}
 	}
 
 }
