ff201636013574d7c7c502f216a49d16c8a07355
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-01-04 16:39:58 +0000

    added all android managers from documentation, and added a usage example

--- HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
+++ HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
@@ -3,7 +3,9 @@ package com.googlecode.androidannotations.helloworldeclipse;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -67,6 +69,14 @@ public class MyActivity extends Activity {
 	void updateUi(String message, int color) {
 		textView.setText(message);
 		textView.setTextColor(color);
+		showNotification();
+	}
+
+	private void showNotification() {
+		Notification notification = new Notification(R.drawable.icon, "Hello !", 0);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(), 0);
+		notification.setLatestEventInfo(getApplicationContext(), "My notification", "Hello World!", contentIntent);
+		notificationManager.notify(1, notification);
 	}
 
 	@Click
