7f904dc6aa3bb31f25210c9ccb72367b299c8b28
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-01-06 17:04:26 +0000

    adding @Transactional

---
+++ src/main/java/com/googlecode/androidannotations/generation/TransactionalInstruction.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2010-2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.googlecode.androidannotations.generation;
+
+import java.util.List;
+
+import com.googlecode.androidannotations.model.Instruction;
+
+public class TransactionalInstruction implements Instruction {
+
+	private static final String FORMAT = //
+	"" + //
+			"    @Override\n" + //
+			"    public %s %s(%s) {\n" + //
+			"        %s.beginTransaction();\n" + //
+			"        try {\n" + //
+			"            %s%s(%s);\n" + //
+			"            %s.setTransactionSuccessful();\n" + //
+			"            return%s;\n" + //
+			"        } catch (RuntimeException e) {\n" + //
+			"        	android.util.Log.e(\"%s\", \"Error in transaction\", e);\n" + //
+			"        	throw e;\n" + //
+			"        } finally {\n" + //
+			"        	%s.endTransaction();\n" + //
+			"        }\n" + //
+			"    }\n" + //
+			"\n";
+
+	private final String methodName;
+
+	private final String className;
+
+	private final List<String> methodArguments;
+
+	private final List<String> methodParameters;
+
+	private final String returnType;
+
+	public TransactionalInstruction(String methodName, String className, List<String> methodArguments, List<String> methodParameters, String returnType) {
+		this.methodName = methodName;
+		this.className = className;
+		this.methodArguments = methodArguments;
+		this.methodParameters = methodParameters;
+		this.returnType = returnType;
+	}
+
+	@Override
+	public String generate() {
+
+		StringBuilder arguments = new StringBuilder();
+		boolean first = true;
+		for (String argument : methodArguments) {
+			if (first) {
+				first = false;
+			} else {
+				arguments.append(", ");
+			}
+			arguments.append(argument);
+		}
+
+		first = true;
+		StringBuilder parameters = new StringBuilder();
+		for (String parameter : methodParameters) {
+			if (first) {
+				first = false;
+			} else {
+				parameters.append(", ");
+			}
+			parameters.append(parameter);
+		}
+
+		String dbParameter = methodParameters.get(0);
+
+		String returnedValue = returnType.equals("void") ? "" : returnType + " result = ";
+		String returnStatement = returnType.equals("void") ? "" : " result";
+
+		return String.format(FORMAT, returnType, methodName, arguments.toString(), dbParameter, returnedValue, methodName, parameters.toString(), dbParameter, returnStatement, className, dbParameter);
+	}
+
+}
---
+++ src/main/java/com/googlecode/androidannotations/processing/TransactionalProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2010-2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.googlecode.androidannotations.processing;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
+
+import com.googlecode.androidannotations.annotations.Transactional;
+import com.googlecode.androidannotations.generation.TransactionalInstruction;
+import com.googlecode.androidannotations.model.Instruction;
+import com.googlecode.androidannotations.model.MetaActivity;
+import com.googlecode.androidannotations.model.MetaModel;
+
+public class TransactionalProcessor implements ElementProcessor {
+
+	@Override
+	public Class<? extends Annotation> getTarget() {
+		return Transactional.class;
+	}
+
+	@Override
+	public void process(Element element, MetaModel metaModel) {
+
+		String methodName = element.getSimpleName().toString();
+
+		Element enclosingElement = element.getEnclosingElement();
+		MetaActivity metaActivity = metaModel.getMetaActivities().get(enclosingElement);
+		String className = metaActivity.getClassSimpleName();
+		List<Instruction> memberInstructions = metaActivity.getMemberInstructions();
+
+		List<String> methodArguments = new ArrayList<String>();
+		List<String> methodParameters = new ArrayList<String>();
+
+		ExecutableElement executableElement = (ExecutableElement) element;
+
+		for (VariableElement parameter : executableElement.getParameters()) {
+			String parameterName = parameter.getSimpleName().toString();
+			String parameterType = parameter.asType().toString();
+			methodArguments.add(parameterType + " " + parameterName);
+			methodParameters.add(parameterName);
+		}
+
+		String returnType = executableElement.getReturnType().toString();
+
+		Instruction instruction = new TransactionalInstruction(methodName, className, methodArguments, methodParameters, returnType);
+		memberInstructions.add(instruction);
+	}
+
+}
