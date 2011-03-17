39275778509e577c3f86f60ea47d8dbad3465888
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-03-17 07:00:33 +0000

    Adding android.view.View import to RoboActivityBodyInstruction, which was missing (issue 25). Adding a sample thats reproduced the problem (SimpleActivity). Thanks to Quentin ambard for spotting the bug and providing the patch. Fixes issue 25

---
+++ RoboGuiceExample/src/com/googlecode/androidannotations/roboguiceexample/SimpleActivity.java
@@ -0,0 +1,11 @@
+package com.googlecode.androidannotations.roboguiceexample;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.Enhance;
+import com.googlecode.androidannotations.annotations.RoboGuice;
+
+@Enhance
+@RoboGuice
+public class SimpleActivity extends Activity{
+}
