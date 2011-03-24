fed9f067e7539efced71e088ca3ec68b6711478f
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-03-24 15:30:46 +0000

    simplest example, for tests

---
+++ HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/SimpleActivity.java
@@ -0,0 +1,10 @@
+package com.googlecode.androidannotations.helloworldeclipse;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.Enhance;
+
+@Enhance(R.layout.my_activity)
+public class SimpleActivity extends Activity{
+
+}
