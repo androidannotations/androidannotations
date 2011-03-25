5d61b73d9743800516d6e80d50cff9b3866318a6
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-03-25 14:43:38 +0000

    implemented @Click, + refactorings

--- src/main/java/com/googlecode/androidannotations/rclass/IRInnerClass.java
+++ src/main/java/com/googlecode/androidannotations/rclass/IRInnerClass.java
@@ -1,13 +1,20 @@
 package com.googlecode.androidannotations.rclass;
 
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JFieldRef;
+
 public interface IRInnerClass {
 
 	boolean containsIdValue(Integer idValue);
 	boolean containsField(String name);
 
 	String getIdQualifiedName(Integer idValue);
-
+	
 	String getIdQualifiedName(String name);
+	
+	JFieldRef getIdStaticRef(Integer idValue, JCodeModel codeModel);
+
+	JFieldRef getIdStaticRef(String name, JCodeModel codeModel);
 	
 	final IRInnerClass EMPTY_R_INNER_CLASS = new RInnerClass(null);
 
