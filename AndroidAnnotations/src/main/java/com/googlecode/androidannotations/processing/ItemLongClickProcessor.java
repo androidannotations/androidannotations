832ae33b8b468ec82dd571d33b908136a53c27f1
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-03 06:15:01 +0000

    Implementation of ItemLongClick with CodeModel

--- AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ItemLongClickProcessor.java
+++ AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ItemLongClickProcessor.java
@@ -33,7 +33,16 @@ import com.googlecode.androidannotations.model.MetaModel;
 import com.googlecode.androidannotations.rclass.IRClass;
 import com.googlecode.androidannotations.rclass.IRInnerClass;
 import com.googlecode.androidannotations.rclass.RClass.Res;
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JFieldRef;
+import com.sun.codemodel.JInvocation;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
 
 /**
  * @author Benjamin Fellous
@@ -96,10 +105,71 @@ public class ItemLongClickProcessor implements ElementProcessor {
 		onCreateInstructions.add(instruction);
 	}
 
-	@Override
-	public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
-		// TODO Auto-generated method stub
-		
-	}
+
+    @Override
+    public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
+        ActivityHolder holder = activitiesHolder.getActivityHolder(element);
+
+        String methodName = element.getSimpleName().toString();
+
+        ExecutableElement executableElement = (ExecutableElement) element;
+        List<? extends VariableElement> parameters = executableElement.getParameters();
+        TypeMirror returnType = executableElement.getReturnType();
+        boolean returnMethodResult = returnType.getKind() != TypeKind.VOID;
+
+        boolean hasItemParameter = parameters.size() == 1;
+        
+        JFieldRef idRef = extractClickQualifiedId(element, codeModel);
+
+        JDefinedClass onItemClickListenerClass = codeModel.anonymousClass(codeModel.ref("android.widget.AdapterView.OnItemLongClickListener"));
+        JMethod onItemLongClickMethod = onItemClickListenerClass.method(JMod.PUBLIC, codeModel.BOOLEAN, "onItemLongClick");
+        JClass adapterViewClass = codeModel.ref("android.widget.AdapterView");
+        JClass viewClass = codeModel.ref("android.view.View");
+        
+        JClass narrowAdapterViewClass = adapterViewClass.narrow(codeModel.wildcard());
+        JVar onItemClickParentParam = onItemLongClickMethod.param(narrowAdapterViewClass, "parent");
+        onItemLongClickMethod.param(viewClass, "view");
+        JVar onItemClickPositionParam = onItemLongClickMethod.param(codeModel.INT, "position");
+        onItemLongClickMethod.param(codeModel.LONG, "id");
+        
+        JBlock onItemLongClickBody = onItemLongClickMethod.body();
+        
+        JInvocation itemClickCall = JExpr.invoke(methodName);
+        
+        if (returnMethodResult) {
+            onItemLongClickBody._return(itemClickCall);
+        } else {
+            onItemLongClickBody.add(itemClickCall);
+            onItemLongClickBody._return(JExpr.TRUE);
+        }
+
+        if (hasItemParameter) {
+            VariableElement parameter = parameters.get(0);
+            String parameterQualifiedName = parameter.asType().toString();
+            itemClickCall.arg(JExpr.cast(codeModel.ref(parameterQualifiedName), JExpr.invoke(onItemClickParentParam, "getAdapter").invoke("getItem").arg(onItemClickPositionParam)));
+        }
+
+        JBlock body = holder.afterSetContentView.body();
+
+        body.add(JExpr.invoke(JExpr.cast(narrowAdapterViewClass, JExpr.invoke("findViewById").arg(idRef)),"setOnItemLongClickListener").arg(JExpr._new(onItemClickListenerClass)));
+    }
+    
+    private JFieldRef extractClickQualifiedId(Element element, JCodeModel codeModel) {
+        ItemLongClick annotation = element.getAnnotation(ItemLongClick.class);
+        int idValue = annotation.value();
+        IRInnerClass rInnerClass = rClass.get(Res.ID);
+        if (idValue == Id.DEFAULT_VALUE) {
+            String fieldName = element.getSimpleName().toString();
+            int lastIndex = fieldName.lastIndexOf("ItemLongClicked");
+            if (lastIndex != -1) {
+                fieldName = fieldName.substring(0, lastIndex);
+            }
+            return rInnerClass.getIdStaticRef(fieldName, codeModel);
+
+        } else {
+            return rInnerClass.getIdStaticRef(idValue, codeModel);
+        }
+    }
+
 
 }
