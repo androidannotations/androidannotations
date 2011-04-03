4e667483c28014418f4be7f3fc01caa8ed3d968a
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-03 06:15:20 +0000

    Implementing @ItemSelect with CodeModel

--- src/main/java/com/googlecode/androidannotations/processing/ItemSelectedProcessor.java
+++ src/main/java/com/googlecode/androidannotations/processing/ItemSelectedProcessor.java
@@ -31,70 +31,141 @@ import com.googlecode.androidannotations.model.MetaModel;
 import com.googlecode.androidannotations.rclass.IRClass;
 import com.googlecode.androidannotations.rclass.IRInnerClass;
 import com.googlecode.androidannotations.rclass.RClass.Res;
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JFieldRef;
+import com.sun.codemodel.JInvocation;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
 
 /**
  * @author Pierre-Yves Ricau
  */
 public class ItemSelectedProcessor implements ElementProcessor {
 
-	private final IRClass rClass;
+    private final IRClass rClass;
 
-	public ItemSelectedProcessor(IRClass rClass) {
-		this.rClass = rClass;
-	}
+    public ItemSelectedProcessor(IRClass rClass) {
+        this.rClass = rClass;
+    }
 
-	@Override
-	public Class<? extends Annotation> getTarget() {
-		return ItemSelect.class;
-	}
+    @Override
+    public Class<? extends Annotation> getTarget() {
+        return ItemSelect.class;
+    }
 
-	@Override
-	public void process(Element element, MetaModel metaModel) {
+    @Override
+    public void process(Element element, MetaModel metaModel) {
 
-		String methodName = element.getSimpleName().toString();
+        String methodName = element.getSimpleName().toString();
 
-		ItemSelect annotation = element.getAnnotation(ItemSelect.class);
-		
-		int idValue = annotation.value();
+        ItemSelect annotation = element.getAnnotation(ItemSelect.class);
 
-		IRInnerClass rInnerClass = rClass.get(Res.ID);
-		String itemClickQualifiedId;
+        int idValue = annotation.value();
 
-		if (idValue == Id.DEFAULT_VALUE) {
-			String fieldName = element.getSimpleName().toString();
-			int lastIndex = fieldName.lastIndexOf("ItemSelected");
-			if (lastIndex != -1) {
-				fieldName = fieldName.substring(0, lastIndex);
-			}
-			itemClickQualifiedId = rInnerClass.getIdQualifiedName(fieldName);
-		} else {
-			itemClickQualifiedId = rInnerClass.getIdQualifiedName(idValue);
-		}
+        IRInnerClass rInnerClass = rClass.get(Res.ID);
+        String itemClickQualifiedId;
 
-		Element enclosingElement = element.getEnclosingElement();
-		MetaActivity metaActivity = metaModel.getMetaActivities().get(enclosingElement);
-		List<Instruction> onCreateInstructions = metaActivity.getOnCreateInstructions();
-		
-		ExecutableElement executableElement = (ExecutableElement) element;
-		List<? extends VariableElement> parameters = executableElement.getParameters();
+        if (idValue == Id.DEFAULT_VALUE) {
+            String fieldName = element.getSimpleName().toString();
+            int lastIndex = fieldName.lastIndexOf("ItemSelected");
+            if (lastIndex != -1) {
+                fieldName = fieldName.substring(0, lastIndex);
+            }
+            itemClickQualifiedId = rInnerClass.getIdQualifiedName(fieldName);
+        } else {
+            itemClickQualifiedId = rInnerClass.getIdQualifiedName(idValue);
+        }
 
-		Instruction instruction;
-		if (parameters.size() == 2) {
-			VariableElement parameter = parameters.get(1);
-			String parameterQualifiedName = parameter.asType().toString();
-			instruction = new ItemSelectedInstruction(methodName, itemClickQualifiedId, parameterQualifiedName);
-		} else {
-			instruction = new ItemSelectedInstruction(methodName, itemClickQualifiedId);
-		}
-		onCreateInstructions.add(instruction);
+        Element enclosingElement = element.getEnclosingElement();
+        MetaActivity metaActivity = metaModel.getMetaActivities().get(enclosingElement);
+        List<Instruction> onCreateInstructions = metaActivity.getOnCreateInstructions();
 
-	}
+        ExecutableElement executableElement = (ExecutableElement) element;
+        List<? extends VariableElement> parameters = executableElement.getParameters();
 
-	@Override
-	public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
-		// TODO Auto-generated method stub
-		
-	}
+        Instruction instruction;
+        if (parameters.size() == 2) {
+            VariableElement parameter = parameters.get(1);
+            String parameterQualifiedName = parameter.asType().toString();
+            instruction = new ItemSelectedInstruction(methodName, itemClickQualifiedId, parameterQualifiedName);
+        } else {
+            instruction = new ItemSelectedInstruction(methodName, itemClickQualifiedId);
+        }
+        onCreateInstructions.add(instruction);
+
+    }
+
+    @Override
+    public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
+        ActivityHolder holder = activitiesHolder.getActivityHolder(element);
+
+        String methodName = element.getSimpleName().toString();
+
+        ExecutableElement executableElement = (ExecutableElement) element;
+        List<? extends VariableElement> parameters = executableElement.getParameters();
+
+        boolean hasItemParameter = parameters.size() == 2;
+
+        JFieldRef idRef = extractClickQualifiedId(element, codeModel);
+
+        JDefinedClass onItemSelectedListenerClass = codeModel.anonymousClass(codeModel.ref("android.widget.AdapterView.OnItemSelectedListener"));
+        JMethod onItemSelectedMethod = onItemSelectedListenerClass.method(JMod.PUBLIC, codeModel.VOID, "onItemSelected");
+        JClass adapterViewClass = codeModel.ref("android.widget.AdapterView");
+        JClass viewClass = codeModel.ref("android.view.View");
+
+        JClass narrowAdapterViewClass = adapterViewClass.narrow(codeModel.wildcard());
+        JVar onItemClickParentParam = onItemSelectedMethod.param(narrowAdapterViewClass, "parent");
+        onItemSelectedMethod.param(viewClass, "view");
+        JVar onItemClickPositionParam = onItemSelectedMethod.param(codeModel.INT, "position");
+        onItemSelectedMethod.param(codeModel.LONG, "id");
+
+        JInvocation itemSelectedCall = onItemSelectedMethod.body().invoke(methodName);
+
+        itemSelectedCall.arg(JExpr.TRUE);
+
+        if (hasItemParameter) {
+            VariableElement parameter = parameters.get(1);
+            String parameterQualifiedName = parameter.asType().toString();
+            itemSelectedCall.arg(JExpr.cast(codeModel.ref(parameterQualifiedName), JExpr.invoke(onItemClickParentParam, "getAdapter").invoke("getItem").arg(onItemClickPositionParam)));
+        }
+
+        JMethod onNothingSelectedMethod = onItemSelectedListenerClass.method(JMod.PUBLIC, codeModel.VOID, "onNothingSelected");
+
+        onNothingSelectedMethod.param(narrowAdapterViewClass, "parent");
+
+        JInvocation nothingSelectedCall = onNothingSelectedMethod.body().invoke(methodName);
+
+        nothingSelectedCall.arg(JExpr.FALSE);
+        if (hasItemParameter) {
+            nothingSelectedCall.arg(JExpr._null());
+        }
+
+        JBlock body = holder.afterSetContentView.body();
+
+        JInvocation findViewById = JExpr.invoke("findViewById");
+        body.add(JExpr.invoke(JExpr.cast(narrowAdapterViewClass, findViewById.arg(idRef)), "setOnItemSelectedListener").arg(JExpr._new(onItemSelectedListenerClass)));
+    }
+
+    private JFieldRef extractClickQualifiedId(Element element, JCodeModel codeModel) {
+        ItemSelect annotation = element.getAnnotation(ItemSelect.class);
+        int idValue = annotation.value();
+        IRInnerClass rInnerClass = rClass.get(Res.ID);
+        if (idValue == Id.DEFAULT_VALUE) {
+            String fieldName = element.getSimpleName().toString();
+            int lastIndex = fieldName.lastIndexOf("ItemSelected");
+            if (lastIndex != -1) {
+                fieldName = fieldName.substring(0, lastIndex);
+            }
+            return rInnerClass.getIdStaticRef(fieldName, codeModel);
+
+        } else {
+            return rInnerClass.getIdStaticRef(idValue, codeModel);
+        }
+    }
 
 }
