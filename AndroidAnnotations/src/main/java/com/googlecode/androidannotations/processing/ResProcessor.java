e10432f6affbee2181bc0b00e86cfbc879417149
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-03 06:15:38 +0000

    Implemented Resource injection with CodeModel

--- AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ResProcessor.java
+++ AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ResProcessor.java
@@ -29,55 +29,82 @@ import com.googlecode.androidannotations.model.MetaModel;
 import com.googlecode.androidannotations.rclass.IRClass;
 import com.googlecode.androidannotations.rclass.IRInnerClass;
 import com.googlecode.androidannotations.rclass.RClass.Res;
+import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JFieldRef;
 
 public class ResProcessor implements ElementProcessor {
 
-	private final IRClass rClass;
-	private final AndroidRes androidValue;
+    private final IRClass rClass;
+    private final AndroidRes androidValue;
 
-	public ResProcessor(AndroidRes androidValue, IRClass rClass) {
-		this.rClass = rClass;
-		this.androidValue = androidValue;
-	}
+    public ResProcessor(AndroidRes androidValue, IRClass rClass) {
+        this.rClass = rClass;
+        this.androidValue = androidValue;
+    }
 
-	@Override
-	public Class<? extends Annotation> getTarget() {
-		return androidValue.getTarget();
-	}
+    @Override
+    public Class<? extends Annotation> getTarget() {
+        return androidValue.getTarget();
+    }
 
-	@Override
-	public void process(Element element, MetaModel metaModel) {
+    @Override
+    public void process(Element element, MetaModel metaModel) {
 
-		String name = element.getSimpleName().toString();
+        String name = element.getSimpleName().toString();
 
-		int idValue = androidValue.idFromElement(element);
+        int idValue = androidValue.idFromElement(element);
 
-		Res resInnerClass = androidValue.getRInnerClass();
+        Res resInnerClass = androidValue.getRInnerClass();
 
-		IRInnerClass rInnerClass = rClass.get(resInnerClass);
-		String qualifiedId;
-		if (idValue == Id.DEFAULT_VALUE) {
-			String fieldName = element.getSimpleName().toString();
-			qualifiedId = rInnerClass.getIdQualifiedName(fieldName);
-		} else {
-			qualifiedId = rInnerClass.getIdQualifiedName(idValue);
-		}
+        IRInnerClass rInnerClass = rClass.get(resInnerClass);
+        String qualifiedId;
+        if (idValue == Id.DEFAULT_VALUE) {
+            String fieldName = element.getSimpleName().toString();
+            qualifiedId = rInnerClass.getIdQualifiedName(fieldName);
+        } else {
+            qualifiedId = rInnerClass.getIdQualifiedName(idValue);
+        }
 
-		Element enclosingElement = element.getEnclosingElement();
-		MetaActivity metaActivity = metaModel.getMetaActivities().get(enclosingElement);
-		List<Instruction> beforeCreateInstructions = metaActivity.getBeforeCreateInstructions();
+        Element enclosingElement = element.getEnclosingElement();
+        MetaActivity metaActivity = metaModel.getMetaActivities().get(enclosingElement);
+        List<Instruction> beforeCreateInstructions = metaActivity.getBeforeCreateInstructions();
 
-		Instruction instruction = new ValueInstruction(name, androidValue.getResourceMethodName(), qualifiedId);
+        Instruction instruction = new ValueInstruction(name, androidValue.getResourceMethodName(), qualifiedId);
 
-		beforeCreateInstructions.add(instruction);
+        beforeCreateInstructions.add(instruction);
 
-	}
+    }
 
-	@Override
-	public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void process(Element element, JCodeModel codeModel, ActivitiesHolder activitiesHolder) {
+        ActivityHolder holder = activitiesHolder.getActivityHolder(element);
+
+        String fieldName = element.getSimpleName().toString();
+
+        int idValue = androidValue.idFromElement(element);
+
+        Res resInnerClass = androidValue.getRInnerClass();
+
+        IRInnerClass rInnerClass = rClass.get(resInnerClass);
+        JFieldRef idRef;
+        if (idValue == Id.DEFAULT_VALUE) {
+            idRef = rInnerClass.getIdStaticRef(fieldName, codeModel);
+        } else {
+            idRef = rInnerClass.getIdStaticRef(idValue, codeModel);
+        }
+
+        JBlock methodBody = holder.beforeSetContentView.body();
+
+        if (holder.resources == null) {
+            holder.resources = methodBody.decl(codeModel.ref("android.content.res.Resources"), "resources_", JExpr.invoke("getResources"));
+        }
+        
+        String resourceMethodName = androidValue.getResourceMethodName();
+        
+        methodBody.assign(JExpr.ref(fieldName), JExpr.invoke(holder.resources, resourceMethodName).arg(idRef));
+
+    }
 
 }
