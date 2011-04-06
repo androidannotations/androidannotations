dd158d67a2a006934a27c0fe4e27788993ea4721
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-06 08:41:49 +0000

    Improved the Maven Eclipse example, so that it works and makes a real usage of AndroidAnnotations. Fixes issue 38.

--- maveneclipse/src/main/java/com/googlecode/androidannotations/maveneclipse/HelloAndroidActivity.java
+++ maveneclipse/src/main/java/com/googlecode/androidannotations/maveneclipse/HelloAndroidActivity.java
@@ -1,11 +1,31 @@
 package com.googlecode.androidannotations.maveneclipse;
 
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.Enhance;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringRes;
 
 @Enhance(R.layout.main)
 public class HelloAndroidActivity extends Activity {
+	
+	@StringRes
+	String hello;
+	
+	@ViewById
+	TextView helloTextView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Date now = new Date();
+		String helloMessage = String.format(hello, now.toString());
+		helloTextView.setText(helloMessage);
+	}
 
 }
 
