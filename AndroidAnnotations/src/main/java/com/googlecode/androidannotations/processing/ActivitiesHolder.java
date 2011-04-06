98d4f37d63365887a189143ef131961760357b00
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-06 12:36:38 +0000

    Using an executor service instead of a new thread each time an @Background method is called. Fixes issue 4. I also had to catch FilerExceptions that happens when creating twice the same file for different rounds...

--- AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ActivitiesHolder.java
+++ AndroidAnnotations/src/main/java/com/googlecode/androidannotations/processing/ActivitiesHolder.java
@@ -5,9 +5,14 @@ import java.util.Map;
 
 import javax.lang.model.element.Element;
 
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JMethod;
+
 public class ActivitiesHolder {
 
     private Map<Element, ActivityHolder> activityHolders = new HashMap<Element, ActivityHolder>();
+	public JMethod executorMethod;
+	public JDefinedClass backgroundExecutor;
 
     public ActivityHolder create(Element activityElement) {
         ActivityHolder activityHolder = new ActivityHolder();
