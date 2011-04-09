84d3214766ff72ae16d712233a141f3993e48cd2
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-09 16:46:26 +0000

    reformatting... bad tabs habit

--- src/main/java/com/googlecode/androidannotations/helper/AndroidManifestFinder.java
+++ src/main/java/com/googlecode/androidannotations/helper/AndroidManifestFinder.java
@@ -20,110 +20,110 @@ import org.w3c.dom.NodeList;
 
 public class AndroidManifestFinder {
 
-	private static final int MAX_PARENTS_FROM_SOURCE_FOLDER = 10;
+    private static final int MAX_PARENTS_FROM_SOURCE_FOLDER = 10;
 
-	private ProcessingEnvironment processingEnv;
+    private ProcessingEnvironment processingEnv;
 
-	public AndroidManifestFinder(ProcessingEnvironment processingEnv) {
-		this.processingEnv = processingEnv;
-	}
+    public AndroidManifestFinder(ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+    }
 
-	public AndroidManifest extractAndroidManifest() {
-		try {
-			return extractAndroidManifestThrowing();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public AndroidManifest extractAndroidManifest() {
+        try {
+            return extractAndroidManifestThrowing();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private AndroidManifest extractAndroidManifestThrowing() throws Exception {
-		File androidManifestFile = findManifestFileThrowing();
-		return parseThrowing(androidManifestFile);
-	}
+    private AndroidManifest extractAndroidManifestThrowing() throws Exception {
+        File androidManifestFile = findManifestFileThrowing();
+        return parseThrowing(androidManifestFile);
+    }
 
-	/**
-	 * We use a dirty trick to find the AndroidManifest.xml file, since it's not
-	 * available in the classpath. The idea is quite simple : create a fake
-	 * class file, retrieve its URI, and start going up in parent folders to
-	 * find the AndroidManifest.xml file. Any better solution will be
-	 * appreciated.
-	 */
-	private File findManifestFileThrowing() throws IOException {
-		Filer filer = processingEnv.getFiler();
+    /**
+     * We use a dirty trick to find the AndroidManifest.xml file, since it's not
+     * available in the classpath. The idea is quite simple : create a fake
+     * class file, retrieve its URI, and start going up in parent folders to
+     * find the AndroidManifest.xml file. Any better solution will be
+     * appreciated.
+     */
+    private File findManifestFileThrowing() throws IOException {
+        Filer filer = processingEnv.getFiler();
 
-		JavaFileObject dummySourceFile = filer.createSourceFile("dummy" + System.currentTimeMillis());
-		String dummySourceFilePath = dummySourceFile.toUri().toString();
+        JavaFileObject dummySourceFile = filer.createSourceFile("dummy" + System.currentTimeMillis());
+        String dummySourceFilePath = dummySourceFile.toUri().toString();
 
-		if (dummySourceFilePath.startsWith("file:")) {
-			dummySourceFilePath = dummySourceFilePath.substring("file:".length());
-		}
+        if (dummySourceFilePath.startsWith("file:")) {
+            dummySourceFilePath = dummySourceFilePath.substring("file:".length());
+        }
 
-		Messager messager = processingEnv.getMessager();
-		messager.printMessage(Kind.NOTE, "Dummy source file: " + dummySourceFilePath);
+        Messager messager = processingEnv.getMessager();
+        messager.printMessage(Kind.NOTE, "Dummy source file: " + dummySourceFilePath);
 
-		File sourcesGenerationFolder = new File(dummySourceFilePath).getParentFile();
+        File sourcesGenerationFolder = new File(dummySourceFilePath).getParentFile();
 
-		File projectRoot = sourcesGenerationFolder.getParentFile();
+        File projectRoot = sourcesGenerationFolder.getParentFile();
 
-		File androidManifestFile = new File(projectRoot, "AndroidManifest.xml");
-		for (int i = 0; i < MAX_PARENTS_FROM_SOURCE_FOLDER; i++) {
-			if (androidManifestFile.exists()) {
-				break;
-			} else {
-				if (projectRoot.getParentFile() != null) {
-					projectRoot = projectRoot.getParentFile();
-					androidManifestFile = new File(projectRoot, "AndroidManifest.xml");
-				} else {
-					break;
-				}
-			}
-		}
+        File androidManifestFile = new File(projectRoot, "AndroidManifest.xml");
+        for (int i = 0; i < MAX_PARENTS_FROM_SOURCE_FOLDER; i++) {
+            if (androidManifestFile.exists()) {
+                break;
+            } else {
+                if (projectRoot.getParentFile() != null) {
+                    projectRoot = projectRoot.getParentFile();
+                    androidManifestFile = new File(projectRoot, "AndroidManifest.xml");
+                } else {
+                    break;
+                }
+            }
+        }
 
-		if (!androidManifestFile.exists()) {
-			throw new IllegalStateException("Could not find the AndroidManifest.xml file, going up from path " + sourcesGenerationFolder.getAbsolutePath() + " found using dummy file [" + dummySourceFilePath + "] (max atempts: " + MAX_PARENTS_FROM_SOURCE_FOLDER + ")");
-		} else {
-			messager.printMessage(Kind.NOTE, "AndroidManifest.xml file found: " + androidManifestFile.toString());
-		}
+        if (!androidManifestFile.exists()) {
+            throw new IllegalStateException("Could not find the AndroidManifest.xml file, going up from path " + sourcesGenerationFolder.getAbsolutePath() + " found using dummy file [" + dummySourceFilePath + "] (max atempts: " + MAX_PARENTS_FROM_SOURCE_FOLDER + ")");
+        } else {
+            messager.printMessage(Kind.NOTE, "AndroidManifest.xml file found: " + androidManifestFile.toString());
+        }
 
-		return androidManifestFile;
-	}
+        return androidManifestFile;
+    }
 
-	private AndroidManifest parseThrowing(File androidManifestFile) throws Exception {
+    private AndroidManifest parseThrowing(File androidManifestFile) throws Exception {
 
-		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-		Document doc = docBuilder.parse(androidManifestFile);
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(androidManifestFile);
 
-		Element documentElement = doc.getDocumentElement();
-		documentElement.normalize();
+        Element documentElement = doc.getDocumentElement();
+        documentElement.normalize();
 
-		String applicationPackage = documentElement.getAttribute("package");
+        String applicationPackage = documentElement.getAttribute("package");
 
-		NodeList activityNodes = documentElement.getElementsByTagName("activity");
+        NodeList activityNodes = documentElement.getElementsByTagName("activity");
 
-		List<String> activityQualifiedNames = new ArrayList<String>();
+        List<String> activityQualifiedNames = new ArrayList<String>();
 
-		for (int i = 0; i < activityNodes.getLength(); i++) {
-			Node activityNode = activityNodes.item(i);
-			Node nameAttribute = activityNode.getAttributes().getNamedItem("android:name");
-			if (nameAttribute != null) {
+        for (int i = 0; i < activityNodes.getLength(); i++) {
+            Node activityNode = activityNodes.item(i);
+            Node nameAttribute = activityNode.getAttributes().getNamedItem("android:name");
+            if (nameAttribute != null) {
 
-				String activityName = nameAttribute.getNodeValue();
-				String activityQualifiedName;
-				if (activityName.startsWith(applicationPackage)) {
-					activityQualifiedName = activityName;
-				} else {
-					if (activityName.startsWith(".")) {
-						activityQualifiedName = applicationPackage + activityName;
-					} else {
-						activityQualifiedName = applicationPackage + "." + activityName;
-					}
-				}
-				activityQualifiedNames.add(activityQualifiedName);
-			}
-		}
+                String activityName = nameAttribute.getNodeValue();
+                String activityQualifiedName;
+                if (activityName.startsWith(applicationPackage)) {
+                    activityQualifiedName = activityName;
+                } else {
+                    if (activityName.startsWith(".")) {
+                        activityQualifiedName = applicationPackage + activityName;
+                    } else {
+                        activityQualifiedName = applicationPackage + "." + activityName;
+                    }
+                }
+                activityQualifiedNames.add(activityQualifiedName);
+            }
+        }
 
-		return new AndroidManifest(applicationPackage, activityQualifiedNames);
-	}
+        return new AndroidManifest(applicationPackage, activityQualifiedNames);
+    }
 
 }
