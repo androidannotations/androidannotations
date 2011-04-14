531ec8ae3c9128b331208e04eec0e24a7a9f7f87
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-14 15:48:22 +0000

    adding a dependency to test androidannotations (issue 51).

---
+++ AndroidAnnotations/android-functional-test-1-5/src/main/java/com/googlecode/androidannotations/test15/HelloAndroidActivity.java
@@ -0,0 +1,10 @@
+package com.googlecode.androidannotations.test15;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main)
+public class HelloAndroidActivity extends Activity {
+}
+
