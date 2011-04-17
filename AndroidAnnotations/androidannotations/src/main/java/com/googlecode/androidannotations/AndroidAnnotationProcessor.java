41b342546d9b4ff03d551a9cd5dea5ade5528f18
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-17 09:53:25 +0000

    formatting with spaces

--- AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/AndroidAnnotationProcessor.java
+++ AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/AndroidAnnotationProcessor.java
@@ -115,199 +115,199 @@ import com.googlecode.androidannotations.validation.ViewByIdValidator;
 import com.sun.codemodel.JCodeModel;
 
 @SupportedAnnotationClasses({ EActivity.class, //
-		BeforeViews.class, //
-		RoboGuice.class, //
-		ViewById.class, //
-		Click.class, //
-		LongClick.class, //
-		ItemClick.class, //
-		ItemLongClick.class, //
-		Touch.class, //
-		ItemSelect.class, //
-		UiThreadDelayed.class, //
-		UiThread.class, //
-		Transactional.class, //
-		Background.class, //
-		Extra.class, //
-		SystemService.class, //
-		SharedPref.class, //
-		StringRes.class, //
-		ColorRes.class, //
-		AnimationRes.class, //
-		BooleanRes.class, //
-		ColorStateListRes.class, //
-		DimensionRes.class, //
-		DimensionPixelOffsetRes.class, //
-		DimensionPixelSizeRes.class, //
-		DrawableRes.class, //
-		IntArrayRes.class, //
-		IntegerRes.class, //
-		LayoutRes.class, //
-		MovieRes.class, //
-		TextRes.class, //
-		TextArrayRes.class, //
-		StringArrayRes.class })
+        BeforeViews.class, //
+        RoboGuice.class, //
+        ViewById.class, //
+        Click.class, //
+        LongClick.class, //
+        ItemClick.class, //
+        ItemLongClick.class, //
+        Touch.class, //
+        ItemSelect.class, //
+        UiThreadDelayed.class, //
+        UiThread.class, //
+        Transactional.class, //
+        Background.class, //
+        Extra.class, //
+        SystemService.class, //
+        SharedPref.class, //
+        StringRes.class, //
+        ColorRes.class, //
+        AnimationRes.class, //
+        BooleanRes.class, //
+        ColorStateListRes.class, //
+        DimensionRes.class, //
+        DimensionPixelOffsetRes.class, //
+        DimensionPixelSizeRes.class, //
+        DrawableRes.class, //
+        IntArrayRes.class, //
+        IntegerRes.class, //
+        LayoutRes.class, //
+        MovieRes.class, //
+        TextRes.class, //
+        TextArrayRes.class, //
+        StringArrayRes.class })
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class AndroidAnnotationProcessor extends AnnotatedAbstractProcessor {
-	
-	@Override
-	public synchronized void init(ProcessingEnvironment processingEnv) {
-		super.init(processingEnv);
-		
-		Messager messager = processingEnv.getMessager();
-		messager.printMessage(Diagnostic.Kind.NOTE, "Starting AndroidAnnotations compile time annotation processing");
-	}
 
-	/**
-	 * We do not need multiple round processing, since the generated classes do
-	 * not need to be processed.
-	 */
-	private boolean alreadyProcessed = false;
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
 
-	@Override
-	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		try {
-			processThrowing(annotations, roundEnv);
-		} catch (Exception e) {
-			handleException(annotations, roundEnv, e);
-		}
-		return true;
-	}
+        Messager messager = processingEnv.getMessager();
+        messager.printMessage(Diagnostic.Kind.NOTE, "Starting AndroidAnnotations compile time annotation processing");
+    }
 
-	private void processThrowing(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) throws IOException {
+    /**
+     * We do not need multiple round processing, since the generated classes do
+     * not need to be processed.
+     */
+    private boolean alreadyProcessed = false;
 
-		if (roundEnv.processingOver() || annotations.size() == 0 || alreadyProcessed) {
-			return;
-		} else {
-			alreadyProcessed = true;
-		}
-		
-		AnnotationElementsHolder extractedModel = extractAnnotations(annotations, roundEnv);
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        try {
+            processThrowing(annotations, roundEnv);
+        } catch (Exception e) {
+            handleException(annotations, roundEnv, e);
+        }
+        return true;
+    }
 
-		AndroidManifest androidManifest = extractAndroidManifest();
+    private void processThrowing(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) throws IOException {
 
-		IRClass rClass = findRClasses(androidManifest);
+        if (roundEnv.processingOver() || annotations.size() == 0 || alreadyProcessed) {
+            return;
+        } else {
+            alreadyProcessed = true;
+        }
 
-		AndroidSystemServices androidSystemServices = new AndroidSystemServices();
+        AnnotationElementsHolder extractedModel = extractAnnotations(annotations, roundEnv);
 
-		AnnotationElements validatedModel = validateAnnotations(extractedModel, rClass, androidSystemServices, androidManifest);
+        AndroidManifest androidManifest = extractAndroidManifest();
 
-		JCodeModel codeModel = processAnnotations(validatedModel, rClass, androidSystemServices, androidManifest);
+        IRClass rClass = findRClasses(androidManifest);
 
-		generateSources(codeModel);
-	}
+        AndroidSystemServices androidSystemServices = new AndroidSystemServices();
 
-	private AndroidManifest extractAndroidManifest() {
-		AndroidManifestFinder finder = new AndroidManifestFinder(processingEnv);
-		return finder.extractAndroidManifest();
-	}
+        AnnotationElements validatedModel = validateAnnotations(extractedModel, rClass, androidSystemServices, androidManifest);
 
-	private AnnotationElementsHolder extractAnnotations(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		ModelExtractor modelExtractor = new ModelExtractor();
-		AnnotationElementsHolder extractedModel = modelExtractor.extract(annotations, roundEnv);
-		return extractedModel;
-	}
+        JCodeModel codeModel = processAnnotations(validatedModel, rClass, androidSystemServices, androidManifest);
 
-	private IRClass findRClasses(AndroidManifest androidManifest) throws IOException {
-		ProjectRClassFinder rClassFinder = new ProjectRClassFinder(processingEnv);
-		IRClass rClass = rClassFinder.find(androidManifest);
+        generateSources(codeModel);
+    }
 
-		AndroidRClassFinder androidRClassFinder = new AndroidRClassFinder(processingEnv);
+    private AndroidManifest extractAndroidManifest() {
+        AndroidManifestFinder finder = new AndroidManifestFinder(processingEnv);
+        return finder.extractAndroidManifest();
+    }
 
-		IRClass androidRClass = androidRClassFinder.find();
+    private AnnotationElementsHolder extractAnnotations(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        ModelExtractor modelExtractor = new ModelExtractor();
+        AnnotationElementsHolder extractedModel = modelExtractor.extract(annotations, roundEnv);
+        return extractedModel;
+    }
 
-		return new CoumpoundRClass(rClass, androidRClass);
-	}
+    private IRClass findRClasses(AndroidManifest androidManifest) throws IOException {
+        ProjectRClassFinder rClassFinder = new ProjectRClassFinder(processingEnv);
+        IRClass rClass = rClassFinder.find(androidManifest);
 
-	private AnnotationElements validateAnnotations(AnnotationElementsHolder extractedModel, IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
-		if (rClass != null) {
-			ModelValidator modelValidator = buildModelValidator(rClass, androidSystemServices, androidManifest);
-			return modelValidator.validate(extractedModel);
-		} else {
-			return EmptyAnnotationElements.INSTANCE;
-		}
-	}
+        AndroidRClassFinder androidRClassFinder = new AndroidRClassFinder(processingEnv);
 
-	private ModelValidator buildModelValidator(IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
-		ModelValidator modelValidator = new ModelValidator();
-		modelValidator.register(new EActivityValidator(processingEnv, rClass, androidManifest));
-		modelValidator.register(new RoboGuiceValidator(processingEnv));
-		modelValidator.register(new ViewByIdValidator(processingEnv, rClass));
-		modelValidator.register(new ClickValidator(processingEnv, rClass));
-		modelValidator.register(new LongClickValidator(processingEnv, rClass));
-		modelValidator.register(new TouchValidator(processingEnv, rClass));
-		modelValidator.register(new ItemClickValidator(processingEnv, rClass));
-		modelValidator.register(new ItemSelectedValidator(processingEnv, rClass));
-		modelValidator.register(new ItemLongClickValidator(processingEnv, rClass));
-		for (AndroidRes androidRes : AndroidRes.values()) {
-			modelValidator.register(new ResValidator(androidRes, processingEnv, rClass));
-		}
-		modelValidator.register(new RunnableValidator(UiThreadDelayed.class, processingEnv));
-		modelValidator.register(new RunnableValidator(UiThread.class, processingEnv));
-		modelValidator.register(new RunnableValidator(Background.class, processingEnv));
-		modelValidator.register(new TransactionalValidator(processingEnv));
-		modelValidator.register(new ExtraValidator(processingEnv));
-		modelValidator.register(new SystemServiceValidator(processingEnv, androidSystemServices));
-		modelValidator.register(new BeforeCreateValidator(processingEnv));
-		modelValidator.register(new SharedPrefValidator(processingEnv));
-		return modelValidator;
-	}
+        IRClass androidRClass = androidRClassFinder.find();
 
-	private JCodeModel processAnnotations(AnnotationElements validatedModel, IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
-		ModelProcessor modelProcessor = buildModelProcessor(rClass, androidSystemServices, androidManifest);
-		return modelProcessor.process(validatedModel);
-	}
+        return new CoumpoundRClass(rClass, androidRClass);
+    }
 
-	private ModelProcessor buildModelProcessor(IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
-		ModelProcessor modelProcessor = new ModelProcessor();
-		modelProcessor.register(new EActivityProcessor(processingEnv, rClass));
-		modelProcessor.register(new RoboGuiceProcessor());
-		modelProcessor.register(new ViewByIdProcessor(rClass));
-		modelProcessor.register(new ClickProcessor(rClass));
-		modelProcessor.register(new LongClickProcessor(rClass));
-		modelProcessor.register(new TouchProcessor(rClass));
-		modelProcessor.register(new ItemClickProcessor(rClass));
-		modelProcessor.register(new ItemSelectedProcessor(rClass));
-		modelProcessor.register(new ItemLongClickProcessor(rClass));
-		for (AndroidRes androidRes : AndroidRes.values()) {
-			modelProcessor.register(new ResProcessor(androidRes, rClass));
-		}
-		modelProcessor.register(new UiThreadProcessor());
-		modelProcessor.register(new UiThreadDelayedProcessor());
-		modelProcessor.register(new BackgroundProcessor());
-		modelProcessor.register(new TransactionalProcessor());
-		modelProcessor.register(new ExtraProcessor());
-		modelProcessor.register(new SystemServiceProcessor(androidSystemServices));
-		modelProcessor.register(new BeforeCreateProcessor());
-		return modelProcessor;
-	}
+    private AnnotationElements validateAnnotations(AnnotationElementsHolder extractedModel, IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
+        if (rClass != null) {
+            ModelValidator modelValidator = buildModelValidator(rClass, androidSystemServices, androidManifest);
+            return modelValidator.validate(extractedModel);
+        } else {
+            return EmptyAnnotationElements.INSTANCE;
+        }
+    }
 
-	private void generateSources(JCodeModel model) throws IOException {
-		CodeModelGenerator modelGenerator = new CodeModelGenerator(processingEnv.getFiler());
-		modelGenerator.generate(model);
-	}
+    private ModelValidator buildModelValidator(IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
+        ModelValidator modelValidator = new ModelValidator();
+        modelValidator.register(new EActivityValidator(processingEnv, rClass, androidManifest));
+        modelValidator.register(new RoboGuiceValidator(processingEnv));
+        modelValidator.register(new ViewByIdValidator(processingEnv, rClass));
+        modelValidator.register(new ClickValidator(processingEnv, rClass));
+        modelValidator.register(new LongClickValidator(processingEnv, rClass));
+        modelValidator.register(new TouchValidator(processingEnv, rClass));
+        modelValidator.register(new ItemClickValidator(processingEnv, rClass));
+        modelValidator.register(new ItemSelectedValidator(processingEnv, rClass));
+        modelValidator.register(new ItemLongClickValidator(processingEnv, rClass));
+        for (AndroidRes androidRes : AndroidRes.values()) {
+            modelValidator.register(new ResValidator(androidRes, processingEnv, rClass));
+        }
+        modelValidator.register(new RunnableValidator(UiThreadDelayed.class, processingEnv));
+        modelValidator.register(new RunnableValidator(UiThread.class, processingEnv));
+        modelValidator.register(new RunnableValidator(Background.class, processingEnv));
+        modelValidator.register(new TransactionalValidator(processingEnv));
+        modelValidator.register(new ExtraValidator(processingEnv));
+        modelValidator.register(new SystemServiceValidator(processingEnv, androidSystemServices));
+        modelValidator.register(new BeforeCreateValidator(processingEnv));
+        modelValidator.register(new SharedPrefValidator(processingEnv));
+        return modelValidator;
+    }
 
-	private void handleException(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv, Exception e) {
-		String errorMessage = "Unexpected error. Please report an issue on AndroidAnnotations, with the following content: " + stackTraceToString(e);
+    private JCodeModel processAnnotations(AnnotationElements validatedModel, IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
+        ModelProcessor modelProcessor = buildModelProcessor(rClass, androidSystemServices, androidManifest);
+        return modelProcessor.process(validatedModel);
+    }
 
-		Messager messager = processingEnv.getMessager();
-		messager.printMessage(Diagnostic.Kind.ERROR, errorMessage);
+    private ModelProcessor buildModelProcessor(IRClass rClass, AndroidSystemServices androidSystemServices, AndroidManifest androidManifest) {
+        ModelProcessor modelProcessor = new ModelProcessor();
+        modelProcessor.register(new EActivityProcessor(processingEnv, rClass));
+        modelProcessor.register(new RoboGuiceProcessor());
+        modelProcessor.register(new ViewByIdProcessor(rClass));
+        modelProcessor.register(new ClickProcessor(rClass));
+        modelProcessor.register(new LongClickProcessor(rClass));
+        modelProcessor.register(new TouchProcessor(rClass));
+        modelProcessor.register(new ItemClickProcessor(rClass));
+        modelProcessor.register(new ItemSelectedProcessor(rClass));
+        modelProcessor.register(new ItemLongClickProcessor(rClass));
+        for (AndroidRes androidRes : AndroidRes.values()) {
+            modelProcessor.register(new ResProcessor(androidRes, rClass));
+        }
+        modelProcessor.register(new UiThreadProcessor());
+        modelProcessor.register(new UiThreadDelayedProcessor());
+        modelProcessor.register(new BackgroundProcessor());
+        modelProcessor.register(new TransactionalProcessor());
+        modelProcessor.register(new ExtraProcessor());
+        modelProcessor.register(new SystemServiceProcessor(androidSystemServices));
+        modelProcessor.register(new BeforeCreateProcessor());
+        return modelProcessor;
+    }
 
-		/*
-		 * Printing exception as an error on a random element. The exception is
-		 * not related to this element, but otherwise it wouldn't show up in
-		 * eclipse.
-		 */
+    private void generateSources(JCodeModel model) throws IOException {
+        CodeModelGenerator modelGenerator = new CodeModelGenerator(processingEnv.getFiler());
+        modelGenerator.generate(model);
+    }
 
-		Element element = roundEnv.getElementsAnnotatedWith(annotations.iterator().next()).iterator().next();
-		messager.printMessage(Diagnostic.Kind.ERROR, errorMessage, element);
-	}
+    private void handleException(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv, Exception e) {
+        String errorMessage = "Unexpected error. Please report an issue on AndroidAnnotations, with the following content: " + stackTraceToString(e);
 
-	private String stackTraceToString(Throwable e) {
-		StringWriter writer = new StringWriter();
-		PrintWriter pw = new PrintWriter(writer);
-		e.printStackTrace(pw);
-		return writer.toString();
-	}
+        Messager messager = processingEnv.getMessager();
+        messager.printMessage(Diagnostic.Kind.ERROR, errorMessage);
+
+        /*
+         * Printing exception as an error on a random element. The exception is
+         * not related to this element, but otherwise it wouldn't show up in
+         * eclipse.
+         */
+
+        Element element = roundEnv.getElementsAnnotatedWith(annotations.iterator().next()).iterator().next();
+        messager.printMessage(Diagnostic.Kind.ERROR, errorMessage, element);
+    }
+
+    private String stackTraceToString(Throwable e) {
+        StringWriter writer = new StringWriter();
+        PrintWriter pw = new PrintWriter(writer);
+        e.printStackTrace(pw);
+        return writer.toString();
+    }
 
 }
--- AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/generation/SourceCodewriter.java
+++ AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/generation/SourceCodewriter.java
@@ -12,50 +12,50 @@ import com.sun.codemodel.JPackage;
 
 public class SourceCodewriter extends CodeWriter {
 
-	private final Filer filer;
+    private final Filer filer;
 
-	private static final VoidOutputStream VOID_OUTPUT_STREAM = new VoidOutputStream();
+    private static final VoidOutputStream VOID_OUTPUT_STREAM = new VoidOutputStream();
 
-	private static class VoidOutputStream extends OutputStream {
-		@Override
-		public void write(int arg0) throws IOException {
-			// Do nothing
-		}
-	}
+    private static class VoidOutputStream extends OutputStream {
+        @Override
+        public void write(int arg0) throws IOException {
+            // Do nothing
+        }
+    }
 
-	public SourceCodewriter(Filer filer) {
-		this.filer = filer;
-	}
+    public SourceCodewriter(Filer filer) {
+        this.filer = filer;
+    }
 
-	@Override
-	public OutputStream openBinary(JPackage pkg, String fileName) throws IOException {
-		String qualifiedClassName = toQualifiedClassName(pkg, fileName);
-		try {
-			JavaFileObject sourceFile = filer.createSourceFile(qualifiedClassName);
-			return sourceFile.openOutputStream();
-		} catch (FilerException e) {
-			/*
-			 * This exception is expected, when some files are created twice. We
-			 * cannot delete existing files, unless using a dirty hack. Files a
-			 * created twice when the same file is created from different
-			 * annotation rounds. Happens when renaming classes, and for
-			 * Background executor. It also probably means I didn't fully
-			 * understand how annotation processing works. If anyone can point
-			 * me out...
-			 */
-			return VOID_OUTPUT_STREAM;
-		}
-	}
+    @Override
+    public OutputStream openBinary(JPackage pkg, String fileName) throws IOException {
+        String qualifiedClassName = toQualifiedClassName(pkg, fileName);
+        try {
+            JavaFileObject sourceFile = filer.createSourceFile(qualifiedClassName);
+            return sourceFile.openOutputStream();
+        } catch (FilerException e) {
+            /*
+             * This exception is expected, when some files are created twice. We
+             * cannot delete existing files, unless using a dirty hack. Files a
+             * created twice when the same file is created from different
+             * annotation rounds. Happens when renaming classes, and for
+             * Background executor. It also probably means I didn't fully
+             * understand how annotation processing works. If anyone can point
+             * me out...
+             */
+            return VOID_OUTPUT_STREAM;
+        }
+    }
 
-	private String toQualifiedClassName(JPackage pkg, String fileName) {
-		int suffixPosition = fileName.lastIndexOf('.');
-		String className = fileName.substring(0, suffixPosition);
+    private String toQualifiedClassName(JPackage pkg, String fileName) {
+        int suffixPosition = fileName.lastIndexOf('.');
+        String className = fileName.substring(0, suffixPosition);
 
-		String qualifiedClassName = pkg.name() + "." + className;
-		return qualifiedClassName;
-	}
+        String qualifiedClassName = pkg.name() + "." + className;
+        return qualifiedClassName;
+    }
 
-	@Override
-	public void close() throws IOException {
-	}
+    @Override
+    public void close() throws IOException {
+    }
 }
