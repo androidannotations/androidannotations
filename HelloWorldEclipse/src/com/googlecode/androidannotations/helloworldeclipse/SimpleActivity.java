5be45704a05fed271abc2be8e4f677adab66d126
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-07-20 10:30:18 +0000

    typos

--- HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/SimpleActivity.java
+++ HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/SimpleActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.my_activity)
-public class SimpleActivity extends Activity{
-
+public class SimpleActivity extends Activity {
+    
+    
 }
