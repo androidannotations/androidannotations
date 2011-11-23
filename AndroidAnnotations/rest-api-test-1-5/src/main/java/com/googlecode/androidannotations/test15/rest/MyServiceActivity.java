a33f759bf2cb6077ed2cb2c542718b7cf12650ac
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-11-23 11:08:25 +0100

    adding an activity

---
+++ AndroidAnnotations/rest-api-test-1-5/src/main/java/com/googlecode/androidannotations/test15/rest/MyServiceActivity.java
@@ -0,0 +1,12 @@
+package com.googlecode.androidannotations.test15.rest;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+import android.app.Activity;
+
+@EActivity
+public class MyServiceActivity extends Activity {
+
+	MyService myService;
+	
+}
