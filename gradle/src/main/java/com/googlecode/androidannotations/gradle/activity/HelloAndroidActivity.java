aec4da27afc8194ae2e4cac8f117cde0a12fb86d
Ealden Esto E. Escanan <ealden@gmail.com>
2011-12-06 00:38:07 +0800

    Convert HelloAndroidActivity to AndroidAnnotations
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- gradle/src/main/java/com/googlecode/androidannotations/gradle/activity/HelloAndroidActivity.java
+++ gradle/src/main/java/com/googlecode/androidannotations/gradle/activity/HelloAndroidActivity.java
@@ -1,24 +1,25 @@
 package com.googlecode.androidannotations.gradle.activity;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringRes;
 import com.googlecode.androidannotations.gradle.R;
 
 import java.util.Date;
 
+@EActivity(R.layout.main)
 public class HelloAndroidActivity extends Activity {
+    @StringRes
     String hello;
+
+    @ViewById
     TextView helloTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        hello = getString(R.string.hello);
-        helloTextView = (TextView) findViewById(R.id.helloTextView);
-
+    @AfterViews
+    void afterViews() {
         Date now = new Date();
         String helloMessage = String.format(hello, now.toString());
         helloTextView.setText(helloMessage);
