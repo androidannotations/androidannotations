afc54755393282a72ff15ad12171cc6b6e543d68
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-01-26 12:46:25 +0100

    removing the factorized method which was hiding information. More explicit this way, even if it brings code repetition

--- AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/validation/ClickValidator.java
+++ AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/validation/ClickValidator.java
@@ -26,6 +26,7 @@ import com.googlecode.androidannotations.helper.IdAnnotationHelper;
 import com.googlecode.androidannotations.helper.IdValidatorHelper;
 import com.googlecode.androidannotations.model.AnnotationElements;
 import com.googlecode.androidannotations.rclass.IRClass;
+import com.googlecode.androidannotations.rclass.IRClass.Res;
 
 public class ClickValidator implements ElementValidator {
 
@@ -46,7 +47,15 @@ public class ClickValidator implements ElementValidator {
 
 		IsValid valid = new IsValid();
 
-		validatorHelper.idListenerMethod(element, validatedElements, valid);
+		validatorHelper.enclosingElementHasEnhancedViewSupportAnnotation(element, validatedElements, valid);
+
+		validatorHelper.idsExists(element, Res.ID, valid);
+
+		validatorHelper.isNotPrivate(element, valid);
+
+		validatorHelper.doesntThrowException((ExecutableElement) element, valid);
+
+		validatorHelper.uniqueId(element, validatedElements, valid);
 
 		ExecutableElement executableElement = (ExecutableElement) element;
 		validatorHelper.returnTypeIsVoid(executableElement, valid);
