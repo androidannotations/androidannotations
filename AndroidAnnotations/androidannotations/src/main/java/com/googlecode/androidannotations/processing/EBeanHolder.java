fc49530909a0675586ff1526fa1afe5b1a29cdd1
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-04-12 16:55:17 +0200

    Replacing Throwable with NoClassDefFoundError, more specific

--- AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EBeanHolder.java
+++ AndroidAnnotations/androidannotations/src/main/java/com/googlecode/androidannotations/processing/EBeanHolder.java
@@ -84,7 +84,7 @@ public class EBeanHolder {
 			JCodeModel codeModel = eBean.owner();
 			try {
 				refClass = codeModel.ref(fullyQualifiedClassName);
-			} catch (Throwable t) {
+			} catch (NoClassDefFoundError ignored) {
 				refClass = codeModel.directClass(fullyQualifiedClassName);
 			}
 			loadedClasses.put(fullyQualifiedClassName, refClass);
