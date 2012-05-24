abc52f752e97244bf80aa8366dcab976b6b30a2b
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-05-24 09:52:50 +0200

    Revert "Update HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyListActivity.java"
    This reverts commit c637999baa31701b28ea4b08ff23af866df335eb.

--- HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyListActivity.java
+++ HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyListActivity.java
@@ -26,6 +26,7 @@ public class MyListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bestFoods);
 		setListAdapter(adapter);
 	}
 
