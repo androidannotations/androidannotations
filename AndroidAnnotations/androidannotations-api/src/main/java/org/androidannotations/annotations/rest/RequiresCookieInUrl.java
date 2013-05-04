d23a232ae9c16def3ad0205eb45717e92a02552d
dhleong <falcone88@gmail.com>
2013-05-04 15:38:50 -0400

    Add new annotations

---
+++ AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/RequiresCookieInUrl.java
@@ -0,0 +1,12 @@
+package org.androidannotations.annotations.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface RequiresCookieInUrl {
+    public String[] value();
+}
---
+++ AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/SetsCookie.java
@@ -0,0 +1,13 @@
+package org.androidannotations.annotations.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface SetsCookie {
+
+    public String[] value();
+}
