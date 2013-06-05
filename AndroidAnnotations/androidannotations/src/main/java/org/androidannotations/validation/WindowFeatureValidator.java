cc78b4d2503bb8b48cc40eaa84fc7cfc51ca8992
Damien <dvilleneuve@excilys.com>
2013-06-05 17:47:49 +0200

    Handle new annotation WindowFeature. See issue #614

---
+++ AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/validation/WindowFeatureValidator.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2010-2013 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.androidannotations.validation;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+
+import org.androidannotations.annotations.WindowFeature;
+import org.androidannotations.helper.TargetAnnotationHelper;
+import org.androidannotations.helper.ValidatorHelper;
+import org.androidannotations.model.AnnotationElements;
+
+public class WindowFeatureValidator implements ElementValidator {
+
+	private ValidatorHelper validatorHelper;
+
+	public WindowFeatureValidator(ProcessingEnvironment processingEnv) {
+		TargetAnnotationHelper annotationHelper = new TargetAnnotationHelper(processingEnv, getTarget());
+		validatorHelper = new ValidatorHelper(annotationHelper);
+	}
+
+	@Override
+	public String getTarget() {
+		return WindowFeature.class.getName();
+	}
+
+	@Override
+	public boolean validate(Element element, AnnotationElements validatedElements) {
+		IsValid valid = new IsValid();
+
+		validatorHelper.hasEActivity(element, validatedElements, valid);
+
+		return valid.isValid();
+	}
+
+}
