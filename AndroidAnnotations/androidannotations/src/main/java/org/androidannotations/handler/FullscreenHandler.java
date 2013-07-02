6dc7a83d1f04b05373d96952fe4cdb12d2d9d126
Yoann Delouis <ydelouis@gmail.com>
2013-07-02 14:08:35 +0200

    Fix bad generated code by FullscreenHandler

--- AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/FullscreenHandler.java
+++ AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/handler/FullscreenHandler.java
@@ -15,15 +15,17 @@
  */
 package org.androidannotations.handler;
 
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
-
+import com.sun.codemodel.JFieldRef;
+import com.sun.codemodel.JInvocation;
 import org.androidannotations.annotations.Fullscreen;
 import org.androidannotations.holder.EActivityHolder;
 import org.androidannotations.model.AnnotationElements;
 import org.androidannotations.process.IsValid;
 
-import com.sun.codemodel.JFieldRef;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+
+import static com.sun.codemodel.JExpr.invoke;
 
 public class FullscreenHandler extends BaseAnnotationHandler<EActivityHolder> {
 
@@ -39,6 +41,7 @@ public class FullscreenHandler extends BaseAnnotationHandler<EActivityHolder> {
 	@Override
 	public void process(Element element, EActivityHolder holder) {
 		JFieldRef fullScreen = classes().WINDOW_MANAGER_LAYOUT_PARAMS.staticRef("FLAG_FULLSCREEN");
-		holder.getInitBody().invoke("getWindow").invoke("setFlags").arg(fullScreen).arg(fullScreen);
+		JInvocation setFlagsInvocation = invoke(invoke("getWindow"), "setFlags").arg(fullScreen).arg(fullScreen);
+		holder.getInitBody().add(setFlagsInvocation);
 	}
 }
