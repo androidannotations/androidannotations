94df9a908a82ff242764ac2f740cee2500b1fb52
Damien <dvilleneuve@excilys.com>
2013-07-12 10:19:32 +0200

    Fix HelloWorldEclipse sample project

--- examples/HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
+++ examples/HelloWorldEclipse/src/com/googlecode/androidannotations/helloworldeclipse/MyActivity.java
@@ -35,104 +35,98 @@ import android.widget.Toast;
 @EActivity(R.layout.my_activity)
 public class MyActivity extends Activity {
 
-    @ViewById
-    EditText myEditText;
+	@ViewById
+	EditText myEditText;
 
-    @ViewById(R.id.myTextView)
-    TextView textView;
+	@ViewById(R.id.myTextView)
+	TextView textView;
 
-    @StringRes(R.string.hello)
-    String helloFormat;
+	@StringRes(R.string.hello)
+	String helloFormat;
 
-    @ColorRes
-    int androidColor;
+	@ColorRes
+	int androidColor;
 
-    @BooleanRes
-    boolean someBoolean;
+	@BooleanRes
+	boolean someBoolean;
 
-    @SystemService
-    NotificationManager notificationManager;
+	@SystemService
+	NotificationManager notificationManager;
 
-    @SystemService
-    WindowManager windowManager;
+	@SystemService
+	WindowManager windowManager;
 
-    /**
-     * AndroidAnnotations gracefully handles support for onBackPressed, whether
-     * you use ECLAIR (2.0), or pre ECLAIR android version.
-     */
-    public void onBackPressed() {
-        Toast.makeText(this, "Back key pressed!", Toast.LENGTH_SHORT).show();
-    }
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // windowManager should not be null
-        windowManager.getDefaultDisplay();
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-    }
+	/**
+	 * AndroidAnnotations gracefully handles support for onBackPressed, whether you use ECLAIR (2.0), or pre ECLAIR android version.
+	 */
+	public void onBackPressed() {
+		Toast.makeText(this, "Back key pressed!", Toast.LENGTH_SHORT).show();
+	}
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// windowManager should not be null
+		windowManager.getDefaultDisplay();
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+	}
 
-    @Click
-    void myButtonClicked() {
-        String name = myEditText.getText().toString();
-        setProgressBarIndeterminateVisibility(true);
-        someBackgroundWork(name, 5);
-    }
+	@Click
+	void myButtonClicked() {
+		String name = myEditText.getText().toString();
+		setProgressBarIndeterminateVisibility(true);
+		someBackgroundWork(name, 5);
+	}
 
-    @Background
-    void someBackgroundWork(String name, long timeToDoSomeLongComputation) {
-        try {
-            TimeUnit.SECONDS.sleep(timeToDoSomeLongComputation);
-        } catch (InterruptedException e) {
-        }
+	@Background
+	void someBackgroundWork(String name, long timeToDoSomeLongComputation) {
+		try {
+			TimeUnit.SECONDS.sleep(timeToDoSomeLongComputation);
+		} catch (InterruptedException e) {
+		}
 
-        String message = String.format(helloFormat, name);
+		String message = String.format(helloFormat, name);
 
-        updateUi(message, androidColor);
+		updateUi(message, androidColor);
 
-        showNotificationsDelayed();
-    }
+		showNotificationsDelayed();
+	}
 
-    @UiThread
-    void updateUi(String message, int color) {
-        setProgressBarIndeterminateVisibility(false);
-        textView.setText(message);
-        textView.setTextColor(color);
-    }
+	@UiThread
+	void updateUi(String message, int color) {
+		setProgressBarIndeterminateVisibility(false);
+		textView.setText(message);
+		textView.setTextColor(color);
+	}
 
-    @UiThread(delay=2000)
-    void showNotificationsDelayed() {
-        Notification notification = new Notification(R.drawable.icon, "Hello !", 0);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(), 0);
-        notification.setLatestEventInfo(getApplicationContext(), "My notification", "Hello World!", contentIntent);
-        notificationManager.notify(1, notification);
-    }
+	@UiThread(delay = 2000)
+	void showNotificationsDelayed() {
+		Notification notification = new Notification(R.drawable.icon, "Hello !", 0);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(), 0);
+		notification.setLatestEventInfo(getApplicationContext(), "My notification", "Hello World!", contentIntent);
+		notificationManager.notify(1, notification);
+	}
 
-    @LongClick
-    void startExtraActivity() {
-        Intent intent = new Intent(this, ActivityWithExtra_.class);
+	@LongClick
+	void startExtraActivity() {
+		Intent intent = ActivityWithExtra_.intent(this).myDate(new Date()).myMessage("hello !").get();
+		intent.putExtra(ActivityWithExtra.MY_INT_EXTRA, 42);
+		startActivity(intent);
+	}
 
-        intent.putExtra(ActivityWithExtra.MY_DATE_EXTRA, new Date());
-        intent.putExtra(ActivityWithExtra.MY_STRING_EXTRA, "hello !");
-        intent.putExtra(ActivityWithExtra.MY_INT_EXTRA, 42);
+	@Click
+	void startListActivity(View v) {
+		startActivity(new Intent(this, MyListActivity_.class));
+	}
 
-        startActivity(intent);
-    }
+	@Touch
+	void myTextView(MotionEvent event) {
+		Log.d("MyActivity", "myTextView was touched!");
+	}
 
-    @Click
-    void startListActivity(View v) {
-        startActivity(new Intent(this, MyListActivity_.class));
-    }
-
-    @Touch
-    void myTextView(MotionEvent event) {
-        Log.d("MyActivity", "myTextView was touched!");
-    }
-
-    @Transactional
-    int transactionalMethod(SQLiteDatabase db, int someParam) {
-        return 42;
-    }
+	@Transactional
+	int transactionalMethod(SQLiteDatabase db, int someParam) {
+		return 42;
+	}
 
 }
