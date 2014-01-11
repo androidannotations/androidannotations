14f0ce69cebc563b1426e193bcf663a3f52db9b1
Damien <dvilleneuve@excilys.com>
2014-01-11 20:57:39 +0100

    Fix javadoc for XXXRes

--- AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/res/TextArrayRes.java
+++ AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/res/TextArrayRes.java
@@ -24,7 +24,7 @@ import org.androidannotations.annotations.ResId;
 
 /**
  * Use on {@link java.lang.CharSequence} array fields in any enhanced classes
- * that should be injected with this specific color resource.
+ * that should be injected with this specific string array resource.
  * <p/>
  * The annotation value must be one of R.array.* fields. If the value is not
  * set, the field name will be used as the R.array.* field name.
