9c00a44daa02bd017ba71b841b24c875406cb53f
Jonathan Maltz <maltz@yelp.com>
2014-04-15 05:49:02 +0800

    added basic docs and license to the header

--- AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/Header.java
+++ AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/Header.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.androidannotations.annotations.rest;
 
 import java.lang.annotation.ElementType;
@@ -5,10 +20,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by jmaltz on 4/15/14.
- */
 
+/**
+ * Use on methods in {@link Rest} annotated class to add headers to a particular method
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Header {
