c9edeaa60b9f33fa3b5079338a1af94097bf8122
Jonathan Maltz <maltz@yelp.com>
2014-04-17 10:07:08 +0800

    added whitespace

--- AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/Header.java
+++ AndroidAnnotations/androidannotations-api/src/main/java/org/androidannotations/annotations/rest/Header.java
@@ -25,7 +25,7 @@ import java.lang.annotation.Target;
  * Use on methods in {@link Rest} annotated class to add headers to a particular method
  *
  * Example usage:
- * 
+ *
  * @Header(headerName="keep-alive", value="300")
  * @Post("/test")
  * public void testRoute()
