1db56c05a3f459768f65ec5d11ee568f7f6604d0
dodgex <kujanssen@freenet.de>
2014-05-12 19:47:56 +0200

    add test for pre set intent that is injected in onCreate()

--- AndroidAnnotations/functional-test-1-5-tests/src/test/java/org/androidannotations/test15/afterextras/AfterExtrasActivityTest.java
+++ AndroidAnnotations/functional-test-1-5-tests/src/test/java/org/androidannotations/test15/afterextras/AfterExtrasActivityTest.java
@@ -17,12 +17,17 @@ package org.androidannotations.test15.afterextras;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.lang.reflect.Field;
+
 import org.androidannotations.test15.AndroidAnnotationsTestRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.app.Activity;
 import android.content.Intent;
+
+import com.xtremelabs.robolectric.shadows.ShadowActivity;
 
 @RunWith(AndroidAnnotationsTestRunner.class)
 public class AfterExtrasActivityTest {
@@ -44,4 +49,17 @@ public class AfterExtrasActivityTest {
 		assertThat(activity.afterExtrasCalled).isTrue();
 	}
 
+	@Test
+	public void afterExtra_called_activity_in_onCreate() throws Exception {
+		Field shadowField = Activity.class.getDeclaredField("__shadow__");
+		Field intentField = ShadowActivity.class.getDeclaredField("intent");
+		intentField.setAccessible(true);
+
+		Object shadow = shadowField.get(activity);
+		intentField.set(shadow, intent);
+
+		activity.onCreate(null);
+		assertThat(activity.extraDataSet).isTrue();
+		assertThat(activity.afterExtrasCalled).isTrue();
+	}
 }
