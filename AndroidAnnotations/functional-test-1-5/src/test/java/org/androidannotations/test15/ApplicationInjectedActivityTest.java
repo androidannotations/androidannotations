a30e52521c520083d5633164af9cad46394a0652
WonderCsabo <kozakcsabi@gmail.com>
2014-09-28 20:30:11 +0200

    Merge test projects
    The functional-test-1-5 and functional-test-1-5-tests are merged into
    one functional-test-1-5 project. This is now possible since the m2e-android
    plugin supports tests within the same project.
    
    The project now uses the fest-android dependency since it provides
    matchers for Android classes. Also it uses fest-assert-core instead of
    fest-assert, so it does not references non-Android classes like
    java.awt.image.BufferedImage.

---
+++ AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/test15/ApplicationInjectedActivityTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.androidannotations.test15;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.androidannotations.test15.roboguice.SampleRoboApplication;
+import org.androidannotations.test15.roboguice.SampleRoboApplication_;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ApplicationInjectedActivityTest {
+	
+	@Test
+	public void should_have_application_after_create() {
+		SampleRoboApplication_ application = new SampleRoboApplication_();
+		application.onCreate();
+		
+		ApplicationInjectedActivity_ activity = Robolectric.buildActivity(ApplicationInjectedActivity_.class).create().get();
+
+		assertThat(activity.customApplication).isNotNull();
+	}
+
+	@Test
+	public void application_can_be_replaced_for_tests() {
+		SampleRoboApplication testApp = new SampleRoboApplication();
+
+		SampleRoboApplication_.setForTesting(testApp);
+
+		ApplicationInjectedActivity_ activity = Robolectric.buildActivity(ApplicationInjectedActivity_.class).create().get();;
+
+		assertThat(activity.customApplication).isSameAs(testApp);
+	}
+
+}
---
+++ AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/test15/ServiceInjectionTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2010-2014 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.androidannotations.test15;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ServiceInjectionTest {
+
+	@Test
+	public void servicesAreInjected() {
+		ActivityWithServices_ activity = Robolectric.buildActivity(ActivityWithServices_.class).create().get();
+
+		assertThat(activity.windowManager).isNotNull();
+		assertThat(activity.layoutInflater).isNotNull();
+		assertThat(activity.activityManager).isNotNull();
+		assertThat(activity.powerManager).isNotNull();
+		assertThat(activity.alarmManager).isNotNull();
+		assertThat(activity.notificationManager).isNotNull();
+		assertThat(activity.keyguardManager).isNotNull();
+		assertThat(activity.locationManager).isNotNull();
+		assertThat(activity.searchManager).isNotNull();
+		assertThat(activity.vibrator).isNotNull();
+		assertThat(activity.connectivityManager).isNotNull();
+		assertThat(activity.wifiManager).isNotNull();
+		assertThat(activity.inputMethodManager).isNotNull();
+		assertThat(activity.sensorManager).isNotNull();
+		assertThat(activity.telephonyManager).isNotNull();
+		assertThat(activity.audioManager).isNotNull();
+		assertThat(activity.textClipboardManager).isNotNull();
+		assertThat(activity.contentClipboardManager).isNotNull();
+	}
+
+}
