0038f254bf3f2fe1da04ecd81f8ac0a3a5152062
Kay-Uwe Janssen <git@kujanssen.de>
2014-11-14 20:55:37 +0100

    do not create a context_ field for `@EReceiver`

--- AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/holder/EReceiverHolder.java
+++ AndroidAnnotations/androidannotations/src/main/java/org/androidannotations/holder/EReceiverHolder.java
@@ -24,14 +24,12 @@ import org.androidannotations.process.ProcessHolder;
 
 import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JExpr;
-import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JInvocation;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JVar;
 
 public class EReceiverHolder extends EComponentHolder {
 
-	private JFieldVar contextField;
 	private JBlock onReceiveBody;
 	private JVar onReceiveIntentAction;
 	private JVar onReceiveIntentDataScheme;
@@ -45,13 +43,15 @@ public class EReceiverHolder extends EComponentHolder {
 
 	@Override
 	protected void setContextRef() {
-		contextField = generatedClass.field(PRIVATE, classes().CONTEXT, "context_");
-		contextRef = contextField;
+		if (init == null) {
+			setInit();
+		}
 	}
 
 	@Override
 	protected void setInit() {
 		init = generatedClass.method(PRIVATE, codeModel().VOID, "init_");
+		contextRef = init.param(classes().CONTEXT, "context");
 		if (onReceiveMethod == null) {
 			createOnReceive();
 		}
@@ -63,8 +63,7 @@ public class EReceiverHolder extends EComponentHolder {
 		onReceiveIntent = onReceiveMethod.param(classes().INTENT, "intent");
 		onReceiveMethod.annotate(Override.class);
 		onReceiveBody = onReceiveMethod.body();
-		onReceiveBody.assign(getContextField(), onReceiveContext);
-		onReceiveBody.invoke(getInit());
+		onReceiveBody.invoke(getInit()).arg(onReceiveContext);
 		onReceiveBody.invoke(JExpr._super(), onReceiveMethod).arg(onReceiveContext).arg(onReceiveIntent);
 
 		JInvocation getActionInvocation = JExpr.invoke(onReceiveIntent, "getAction");
@@ -113,12 +112,5 @@ public class EReceiverHolder extends EComponentHolder {
 			createOnReceive();
 		}
 		return onReceiveIntentDataScheme;
-	}
-
-	public JFieldVar getContextField() {
-		if (contextField == null) {
-			setContextRef();
-		}
-		return contextField;
 	}
 }
