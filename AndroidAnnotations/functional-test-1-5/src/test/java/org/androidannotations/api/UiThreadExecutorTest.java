f0478d7bd67c86aa7b65db3c2d729b07601db416
Drozdov Artyom <artem.drozdov@corp.mail.ru>
2015-04-19 22:08:55 +0300

    simple and effective implementation of cancelling ui tasks

--- AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/api/UiThreadExecutorTest.java
+++ AndroidAnnotations/functional-test-1-5/src/test/java/org/androidannotations/api/UiThreadExecutorTest.java
@@ -1,30 +1,51 @@
 package org.androidannotations.api;
 
-import org.androidannotations.api.UiThreadExecutor;
+import android.os.Handler;
+import android.os.Message;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowHandler;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class UiThreadExecutorTest {
 
+	//remove this after upgrading robolectric to 3+
+	@BeforeClass
+	public static void hackOldRobolectric() {
+		final Handler handler = UiThreadExecutor.HANDLER;
+		ShadowHandler shadowHandler = Robolectric. shadowOf_(handler);
+		shadowHandler.__constructor__(new Handler.Callback() {
+			@Override
+			public boolean handleMessage(Message msg) {
+				//in the robolectric 2.4 there is a strange code - they call Handler's handleMessage (it do nothing)
+				//instead of dispatch, that actually do the job. This is just a dirty work-around. It should be removed
+				//with newer version of robolectric.
+				handler.dispatchMessage(msg);
+				return true;
+			}
+		});
+	}
+
 	@Test
 	public void oneTaskTest() throws Exception {
-		final CountDownLatch latch = new CountDownLatch(1);
+		final AtomicBoolean done = new AtomicBoolean(false);
 		UiThreadExecutor.runTask("test", new Runnable() {
 			@Override
 			public void run() {
-				UiThreadExecutor.done("test", this);
-				latch.countDown();
+				done.set(true);
 			}
 		}, 0);
-		await(latch);
-		assertTrue("The task is leaked", UiThreadExecutor.TASKS.isEmpty());
+		Robolectric.runUiThreadTasksIncludingDelayedTasks();
+		assertTrue("Task is still under execution", done.get());
 	}
 
 	@Test
@@ -38,16 +59,14 @@ public class UiThreadExecutorTest {
 					@Override
 					public void run() {
 						await(taskStartedLatch);
-						assertFalse("There is no tasks in map", UiThreadExecutor.TASKS.isEmpty());
-						UiThreadExecutor.done("test", this);
 						taskFinishedLatch.countDown();
 					}
 				}, 0);
 				taskStartedLatch.countDown();
-				await(taskFinishedLatch);
-				assertTrue("The task is leaked", UiThreadExecutor.TASKS.isEmpty());
+				Robolectric.runUiThreadTasksIncludingDelayedTasks();
 			}
 		}.start();
+		await(taskFinishedLatch);
 	}
 
 	private void await(CountDownLatch latch) {
