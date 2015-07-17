0bc449e2fbbaa1b0d1acdc6f6749906b04d4bb4e
yDelouis <ydelouis@gmail.com>
2015-07-17 20:31:56 +0200

    Fix typo

--- AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/ModelConstants.java
+++ AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/ModelConstants.java
@@ -54,11 +54,11 @@ public abstract class ModelConstants {
 		classSuffix = environment.getOptionValue(OPTION_CLASS_SUFFIX).trim();
 
 		if (classSuffix.isEmpty()) {
-			throw new IllegalArgumentException("'" + classSuffix + "' may not be an emtpy string.");
+			throw new IllegalArgumentException("'" + classSuffix + "' may not be an empty string.");
 		}
 
 		if (!SourceVersion.isName(classSuffix) || classSuffix.contains(".")) {
-			throw new IllegalArgumentException("'" + classSuffix + "' is not a valid Java identifier.");
+			throw new IllegalArgumentException("'" + classSuffix + "' may not be a valid Java identifier.");
 		}
 	}
 
