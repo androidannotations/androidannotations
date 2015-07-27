36fafbf6de51861c8db76d22cd59147564c83ec7
WonderCsabo <kozakcsabi@gmail.com>
2015-07-27 14:05:13 +0200

    Move Handler hack to core test module

--- AndroidAnnotations/androidannotations-roboguice/roboguice-test/src/test/java/org/androidannotations/roboguice/test/TestSampleRoboApplication_.java
+++ AndroidAnnotations/androidannotations-roboguice/roboguice-test/src/test/java/org/androidannotations/roboguice/test/TestSampleRoboApplication_.java
@@ -1,22 +1,15 @@
 package org.androidannotations.roboguice.test;
 
-import static org.fest.reflect.core.Reflection.staticField;
+import android.app.Application;
+import org.robolectric.Robolectric;
+import org.robolectric.TestLifecycleApplication;
+import roboguice.RoboGuice;
 
 import java.lang.reflect.Method;
 
-import org.androidannotations.api.UiThreadExecutor;
-import org.robolectric.Robolectric;
-import org.robolectric.TestLifecycleApplication;
-import org.robolectric.shadows.ShadowHandler;
-
-import roboguice.RoboGuice;
-import android.app.Application;
-import android.os.Handler;
-import android.os.Message;
-
 // CHECKSTYLE:OFF
 public class TestSampleRoboApplication_ extends Application implements TestLifecycleApplication {
-
+	// CHECKSTYLE:ON
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -35,29 +28,9 @@ public class TestSampleRoboApplication_ extends Application implements TestLifec
 		RoboGuice.overrideApplicationInjector(application, RoboGuice.newDefaultRoboModule(application), new RobolectricSampleTestModule());
 
 		RoboGuice.getInjector(application).injectMembers(test);
-
-		hackHandler();
 	}
 
 	@Override
 	public void afterTest(Method method) {
-	}
-
-	// TODO remove this after upgrading robolectric to 3+
-	private void hackHandler() {
-		final Handler handler = staticField("HANDLER").ofType(Handler.class).in(UiThreadExecutor.class).get();
-		ShadowHandler shadowHandler = Robolectric.shadowOf_(handler);
-		shadowHandler.__constructor__(new Handler.Callback() {
-			@Override
-			public boolean handleMessage(Message msg) {
-				// in the robolectric 2.4 there is a strange code - they call
-				// Handler's handleMessage (it do nothing)
-				// instead of dispatch, that actually do the job. This is just a
-				// dirty work-around. It should be removed
-				// with newer version of robolectric.
-				handler.dispatchMessage(msg);
-				return true;
-			}
-		});
 	}
 }
