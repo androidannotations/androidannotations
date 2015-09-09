cac49907ca64cfd0f7264ebd3ef4b9de982c6652
Péter Simon <simonpeterferenc@gmail.com>
2015-09-09 07:58:22 +0200

    Tests for @Rest responseErrorHandler parameter

---
+++ AndroidAnnotations/androidannotations-rest-spring/rest-spring/src/test/java/org/androidannotations/rest/spring/RestResponseErrorHandlerTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2010-2015 eBusiness Information, Excilys Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.androidannotations.rest.spring;
+
+import java.io.IOException;
+
+import org.androidannotations.internal.AndroidAnnotationProcessor;
+import org.androidannotations.testutils.AAProcessorTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestResponseErrorHandlerTest extends AAProcessorTestHelper {
+
+	@Before
+	public void setUp() {
+		addManifestProcessorParameter(RestInterceptorTest.class);
+		addProcessor(AndroidAnnotationProcessor.class);
+	}
+
+	@Test
+	public void clientWithValidResponseErrorHandlerCompiles() {
+		CompileResult result = compileFiles(ClientWithValidResponseErrorHandler.class);
+		assertCompilationSuccessful(result);
+	}
+
+	@Test
+	public void clientWithNonResponseErrorHandlerDoesNotCompile() throws IOException {
+		CompileResult result = compileFiles(ClientWithNonResponseErrorHandler.class);
+		assertCompilationErrorOn(ClientWithNonResponseErrorHandler.class, "@Rest", result);
+	}
+
+	@Test
+	public void clientWithWrongConstructorResponseErrorHandlerDoesNotCompile() throws IOException {
+		CompileResult result = compileFiles(ClientWithWrongConstructorResponseErrorHandler.class);
+		assertCompilationErrorOn(ClientWithWrongConstructorResponseErrorHandler.class, "@Rest", result);
+	}
+
+	@Test
+	public void clientWithAbstractResponseErrorHandlerDoesNotCompile() throws IOException {
+		CompileResult result = compileFiles(ClientWithAbstractResponseErrorHandler.class);
+		assertCompilationErrorOn(ClientWithAbstractResponseErrorHandler.class, "@Rest", result);
+	}
+	@Test
+	public void clientWithNonClassResponseErrorHandlerDoesNotCompile() throws IOException {
+		CompileResult result = compileFiles(ClientWithNonClassResponseErrorHandler.class);
+		assertCompilationErrorOn(ClientWithNonClassResponseErrorHandler.class, "@Rest", result);
+	}
+}
