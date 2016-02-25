androidannotations/androidannotations

commit 240c51e8f4f2a23d1128e17a81eab089aefca2af

Properly override varags methods

AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
@@ -212,8 +212,11 @@ public class APTCodeModelHelper {
 			method.generify(typeDeclaration.getKey(), typeDeclaration.getValue());
 		}
 
+		int i = 0;
 		for (VariableElement parameter : executableElement.getParameters()) {
-			addParamToMethod(method, parameter, JMod.FINAL, actualTypes);
+			boolean varParam = i == executableElement.getParameters().size() - 1 && executableElement.isVarArgs();
+			addParamToMethod(method, parameter, JMod.FINAL, actualTypes, varParam);
+			i++;
 		}
 
 		for (TypeMirror superThrownType : executableElement.getThrownTypes()) {
@@ -266,10 +269,10 @@ public class APTCodeModelHelper {
 		return null;
 	}
 
-	private void addParamToMethod(JMethod method, VariableElement parameter, int mod, Map<String, TypeMirror> actualTypes) {
+	private void addParamToMethod(JMethod method, VariableElement parameter, int mod, Map<String, TypeMirror> actualTypes, boolean varParam) {
 		String parameterName = parameter.getSimpleName().toString();
 		JClass parameterClass = typeMirrorToJClass(parameter.asType(), actualTypes);
-		JVar param = method.param(mod, parameterClass, parameterName);
+		JVar param = varParam ? method.varParam(parameterClass.elementType(), parameterName) : method.param(mod, parameterClass, parameterName);
 		copyNonAAAnnotations(param, parameter.getAnnotationMirrors());
 	}
 

commit 910485198725899b17bfd86d44d7bad3d82c36e9

Make varparams final in overriden methods
This fixes a regression introduced in 240c51e8.
We use the dedicated JMethod#varParam() API to add the varparam to
the overriden method signature, however the `final` modifier was left
out. Now we add the `final` modifier to variable length parameters as
 well.

AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
@@ -309,7 +309,7 @@ public class APTCodeModelHelper {
 	private void addParamToMethod(JMethod method, VariableElement parameter, int mod, Map<String, TypeMirror> actualTypes, boolean varParam) {
 		String parameterName = parameter.getSimpleName().toString();
 		AbstractJClass parameterClass = typeMirrorToJClass(parameter.asType(), actualTypes);
-		JVar param = varParam ? method.varParam(parameterClass.elementType(), parameterName) : method.param(mod, parameterClass, parameterName);
+		JVar param = varParam ? method.varParam(mod, parameterClass.elementType(), parameterName) : method.param(mod, parameterClass, parameterName);
 		copyNonAAAnnotations(param, parameter.getAnnotationMirrors());
 	}
 