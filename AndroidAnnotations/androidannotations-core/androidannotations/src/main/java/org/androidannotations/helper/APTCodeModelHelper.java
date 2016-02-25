androidannotations/androidannotations

commit 240c51e8f4f2a23d1128e17a81eab089aefca2af

Properly override varags methods

AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
@@ -212,8 +212,11 @@ public class APTCodeModelHelper {
 			method.generify(typeDeclaration.getKey(), typeDeclaration.getValue());
 		}
 
+		int i = 0;
 		for (VariableElement parameter : executableElement.getParameters()) {
-			addParamToMethod(method, parameter, JMod.FINAL, actualTypes);
+			boolean varParam = i == executableElement.getParameters().size() - 1 && executableElement.isVarArgs();
+			addParamToMethod(method, parameter, JMod.FINAL, actualTypes, varParam);
+			i++;
 		}
 
 		for (TypeMirror superThrownType : executableElement.getThrownTypes()) {
@@ -266,10 +269,10 @@ public class APTCodeModelHelper {
 		return null;
 	}
 
-	private void addParamToMethod(JMethod method, VariableElement parameter, int mod, Map<String, TypeMirror> actualTypes) {
+	private void addParamToMethod(JMethod method, VariableElement parameter, int mod, Map<String, TypeMirror> actualTypes, boolean varParam) {
 		String parameterName = parameter.getSimpleName().toString();
 		JClass parameterClass = typeMirrorToJClass(parameter.asType(), actualTypes);
-		JVar param = method.param(mod, parameterClass, parameterName);
+		JVar param = varParam ? method.varParam(parameterClass.elementType(), parameterName) : method.param(mod, parameterClass, parameterName);
 		copyNonAAAnnotations(param, parameter.getAnnotationMirrors());
 	}
 

commit d5a1874b4d38a48d8f7e2458f18fe7f50151be59

Add missing varparam to super call when overriding
This commits fixes a regression introduced in 240c51e8.
We use the dedicated JMethod#varParam() API to add the variable length
parameter, however we only added normal parameters to the super
call. Now we also add the dedicated varparam to the super call as well.

AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/helper/APTCodeModelHelper.java
@@ -362,6 +362,10 @@ public class APTCodeModelHelper {
 			superCall.arg(param);
 		}
 
+		if (superMethod.hasVarArgs()) {
+			superCall.arg(superMethod.varParam());
+		}
+
 		return superCall;
 	}
 