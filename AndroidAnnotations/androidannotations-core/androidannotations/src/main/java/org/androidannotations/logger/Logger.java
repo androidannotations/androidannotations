045c7b9802225ee52afc110e92e4fc96aae822a6
Kay-Uwe Janssen <git@kujanssen.de>
2018-06-10 09:13:03 +0200

    Remove unused `warn` overload

--- AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/logger/Logger.java
+++ AndroidAnnotations/androidannotations-core/androidannotations/src/main/java/org/androidannotations/logger/Logger.java
@@ -45,10 +45,6 @@ public class Logger {
 		warn(message, null, null, args);
 	}
 
-	public void warn(String message, Throwable thr, Object... args) {
-		warn(message, null, thr, args);
-	}
-
 	public void warn(String message, Element element, Object... args) {
 		warn(message, element, null, args);
 	}
